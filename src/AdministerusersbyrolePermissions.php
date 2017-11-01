78891737801b6c7bf3d73d59cb81dd3f6e0ecf40
AdamPS <id@ashepherd.co.uk>
2017-11-01 23:51:27 +0000

    Remove unused $entityManager

--- src/AdministerusersbyrolePermissions.php
+++ src/AdministerusersbyrolePermissions.php
@@ -3,7 +3,6 @@
 namespace Drupal\administerusersbyrole;
 
 use Drupal\Core\DependencyInjection\ContainerInjectionInterface;
-use Drupal\Core\Entity\EntityManagerInterface;
 use Drupal\Core\StringTranslation\StringTranslationTrait;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Drupal\Core\Session\AccountInterface;
@@ -16,27 +15,10 @@ class AdministerusersbyrolePermissions implements ContainerInjectionInterface {
   use StringTranslationTrait;
 
   /**
-   * The entity manager.
-   *
-   * @var \Drupal\Core\Entity\EntityManagerInterface
-   */
-  protected $entityManager;
-
-  /**
-   * Constructs a new AdministerusersbyrolePermissions instance.
-   *
-   * @param \Drupal\Core\Entity\EntityManagerInterface $entity_manager
-   *   The entity manager.
-   */
-  public function __construct(EntityManagerInterface $entity_manager) {
-    $this->entityManager = $entity_manager;
-  }
-
-  /**
    * {@inheritdoc}
    */
   public static function create(ContainerInterface $container) {
-    return new static($container->get('entity.manager'));
+    return new static();
   }
 
   /**
